0540c85b3c14c56e7de3caf28e4a8d49f629797f
astralnuke <a.trejo.cien@gmail.com>
2024-08-02 02:22:27 -0600

    I continue workind in tasks CRUD

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,7 +1,60 @@
-export const getTasks = async (req, res) => {}
-export const getTask = async (req, res) => {}
-export const createTask = async (req, res) => {}
-export const deleteTask = async (req, res) => {}
-export const updateTask = async (req, res) => {}
+import Task from '../models/task.model.js'
 
+export const getTasks = async (req, res) => {
+  const tasks = await Task.find({
+    user: req.user.id
+  }).populate('user')
+  console.log(tasks)
+  res.json(tasks)
+}
 
+export const getTask = async (req, res) => {
+  const { id } = req.params
+
+  const findedTask = await Task.findById(id, {
+    user: req.user.id
+  }).populate('user')
+
+  if (!findedTask) return res.status(404).json({ message: 'Task not found' })
+
+  res.json(findedTask)
+}
+
+export const createTask = async (req, res) => {
+  const { title, description, date } = req.body
+
+  const newTask = new Task({
+    title,
+    description,
+    date,
+    user: req.user.id
+  })
+
+  const savedTask = await newTask.save()
+  res.json(savedTask)
+}
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params
+
+  const findedTask = await Task.findByIdAndDelete(id)
+
+  if (!findedTask) return res.status(404).json({ message: 'Task not found' })
+
+  res.status(204)
+}
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params
+  const { title, description, date } = req.body
+
+  const findedTask = await Task.findByIdAndUpdate(
+    id,
+    { title, description, date },
+    { new: true }
+  )
+
+  if (!findedTask) return res.status(404).json({ message: 'Task not found' })
+
+  res.json(findedTask)
+}
--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -4,8 +4,10 @@ import { createTask, updateTask, deleteTask, getTask, getTasks } from '../contro
 
 const router = new Router()
 
-router.get('/tasks', getTasks)
-router.get('/tasks/:id', getTask)
-router.post('/tasks', createTask)
-router.delete('/tasks/:id', deleteTask)
-router.put('/tasks/:id', updateTask)
+router.get('/tasks', authRequired, getTasks)
+router.get('/tasks/:id', authRequired, getTask)
+router.post('/tasks', authRequired, createTask)
+router.delete('/tasks/:id', authRequired, deleteTask)
+router.put('/tasks/:id', authRequired, updateTask)
+
+export default router
